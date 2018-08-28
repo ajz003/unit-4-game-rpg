1f97dad82b0f7b2dbaa935e4814e6c22ecd58cf8
ajz003 <ajz003@ucla.edu>
2018-08-27 22:45:04 -0700

    First final draft with added sound effects

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,25 @@
 $(document).ready(function () {
 
+    // ----------- app state (sound effects)
 
+    var attack1 = new Audio("assets/audio/clash1.wav");
+    var attack2 = new Audio("assets/audio/clash2.wav");
+    var attack3 = new Audio("assets/audio/clash3.wav");
+    var attack4 = new Audio("assets/audio/clash4.wav");
 
+    var start = new Audio("assets/audio/start.wav");
+    var start2 = new Audio("assets/audio/enemy.wav");
 
-    // ----------- app state
+    var loss1 = new Audio("assets/audio/laughfuzzball.mp3");
+    var loss2 = new Audio("assets/audio/WilhelmScream.mp3");
+    var loss3 = new Audio("assets/audio/chosenone.mp3");
+
+    var victory = new Audio("assets/audio/star-wars-theme.mp3");
+
+    var attackSounds = [attack1, attack2, attack3, attack4];
+    var lossSounds = [loss1, loss2, loss3];
+
+    // ----------- app state (character stats)
 
     var timesAttacked = 0
 
@@ -11,7 +27,7 @@ $(document).ready(function () {
         name: "easy",
         pronoun: "Darth Sidious",
         healthPoints: 100,
-        attackPower: 100,
+        attackPower: 75,
         counterAttackPower: 50,
         attackPowerGrowth: 25
     };
@@ -45,17 +61,14 @@ $(document).ready(function () {
     var characterArr = [easy, normal, heroic, legendary];
     var characterNameArr = [easy.name, normal.name, heroic.name, legendary.name];
 
-    var isCharacterSelected = Boolean(false);
-    var isEnemySelected = Boolean(false);
-    var gameOver = Boolean(false);
+    // ----------- app state (character selections + Text Set-up)
+
+    var isCharacterSelected = false;
+    var isEnemySelected = false;
+    var gameOver = false;
     var yourCharacter = undefined;
     var opponentCharacter = undefined;
     var wins = 0
-
-
-
-
-
 
     $("#easy-hp").text(easy.healthPoints)
     $("#normal-hp").text(normal.healthPoints)
@@ -65,17 +78,9 @@ $(document).ready(function () {
     $("#game-notes").css("visibility", "hidden")
     $("#restart").css("visibility", "hidden")
 
-
-
-    // ----------- the character clicking
-
+    // ----------- Character Select
 
     $(".character").click(function (e) {
-
-
-        console.log("This.parent: "+this.parent)
-
-
 
         if (isCharacterSelected === false) {
 
@@ -85,16 +90,14 @@ $(document).ready(function () {
             $("#your-character").append($(this).parent())
 
             $($(".enemy-available").parent()).appendTo("#enemies-available");
-            
+
             $("#your-character").find("*").css("border-color", "#7578ff")
             $("#enemies-available").find("*").css("border-color", "#88ff4f")
 
-
+            start.play();
 
             for (var i = 0; i < characterNameArr.length; i++) {
                 var you = $(this).attr("id")
-                console.log("you: " + you);
-                console.log("characterNameArr: " + characterNameArr[i]);
 
                 if (characterNameArr[i] === you) {
                     yourCharacter = characterArr[i];
@@ -103,21 +106,24 @@ $(document).ready(function () {
 
             }
 
-            console.log("you final: " + yourCharacter.name)
             isCharacterSelected = true;
 
         }
 
+        // ----------- Opponent Select
+
         if (isEnemySelected === false && $(this).hasClass("enemy-available")) {
+
+            $("#game-notes").css("visibility", "visible")
 
             $(this).addClass("enemy-current");
             $("#defender").append($(this).parent());
             $("#defender").find("*").css("border-color", "red")
 
+            start2.play();
+
             for (var i = 0; i < characterNameArr.length; i++) {
                 var you = $(this).attr("id")
-                console.log("you: " + you);
-                console.log("characterNameArr: " + characterNameArr[i]);
 
                 if (characterNameArr[i] === you) {
                     opponentCharacter = characterArr[i];
@@ -125,55 +131,42 @@ $(document).ready(function () {
                 }
 
             }
-
-            $("#game-notes").css("visibility", "visible")
-
-            console.log("opponent final: " + opponentCharacter.name)
-            $("#game-notes").text("You have challenged " + opponentCharacter.pronoun + " . May the Force be with you.")
-
             isEnemySelected = true;
 
-            
-
-
+            $("#game-notes").text("You have challenged " + opponentCharacter.pronoun + " . May the Force be with you.")
 
         }
 
-
     });
 
-    // ----------- the attack button
-
+    // ----------- Attack Button
 
     $(".btn").click(function (e) {
-
-        console.log(isCharacterSelected)
-        console.log(isEnemySelected)
-        console.log(gameOver)
 
         var you = yourCharacter
         var opponent = opponentCharacter
 
         if (isCharacterSelected === true && isEnemySelected === true && gameOver === false && opponent.healthPoints > 0 && you.healthPoints > 0) {
 
-            $("#game-notes").show()
+            var x = (Math.floor(Math.random() * 4))
+            attackSounds[x].play();
 
             timesAttacked++
 
-
-            legendary.attackPowerGrowth = you.attackPower
+            legendary.attackPowerGrowth = you.attackPower // This gives Luke exponential Attack Power Growth!
 
             opponent.healthPoints -= you.attackPower;
             you.healthPoints -= opponent.counterAttackPower;
-            $("#game-notes").text("You attacked " + opponent.pronoun + " for " + you.attackPower + " damage. " + opponent.pronoun + " attacked you back for " + opponent.counterAttackPower + " damage.")
+            $("#game-notes").text("You attacked " + opponent.pronoun + " for " + you.attackPower + " damage. " + opponent.pronoun +
+                " attacked you back for " + opponent.counterAttackPower + " damage.")
             you.attackPower += you.attackPowerGrowth;
-
-        
 
             $("#easy-hp").text(easy.healthPoints)
             $("#normal-hp").text(normal.healthPoints)
             $("#heroic-hp").text(heroic.healthPoints)
             $("#legendary-hp").text(legendary.healthPoints)
+
+            // ----------- Winning and Losing
 
             if (opponent.healthPoints <= 0 && you.healthPoints > 0 && gameOver === false) {
                 wins++;
@@ -185,44 +178,41 @@ $(document).ready(function () {
                 }
             }
 
-        }
-
-        else {
+        } else if (gameOver === false) {
             $("#game-notes").text("No enemy here.")
         }
 
-        if (you.healthPoints <= 0) {
+        if (you.healthPoints <= 0 && gameOver === false) {
+            var x = (Math.floor(Math.random() * 3))
+
             $("#game-notes").text("You lose.")
-            gameOver === true;
+
+            setTimeout(function () {
+                lossSounds[x].play();
+            }, 1400);
             $("#restart").css("visibility", "visible")
+            gameOver = true;
 
         }
 
         if (wins === 3) {
             $("#game-notes").text("You win!")
-            gameOver === true;
+            setTimeout(function () {
+                victory.play();
+            }, 1500);
+
             $("#restart").css("visibility", "visible")
-
+            gameOver = true;
         }
-
 
     });
 
+    // ----------- Restart Button
 
     $("#restart").click(function (e) {
 
         window.location.reload();
 
     });
-
-    // ----------- functions
-
-
-
-
-
-    // --------- initialize application
-
-
 
 });
